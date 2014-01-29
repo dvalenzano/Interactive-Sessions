37814e132aa907a62849fadcad2876d614eb7975
dario valenzano <drvalenzano@gmail.com>
2014-01-29 17:12:58 +0100

    Create transcriptome_compression.py

---
+++ transcriptome_compression.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+# <nbformat>3.0</nbformat>
+
+# <codecell>
+
+tr = open('/Volumes/group_dv/group/data/sequences/transcriptome/Annotated_Reference_Nfurzeri_transcriptome_Liver_Testis_Brain.fa', 'rU').read()
+
+# <codecell>
+
+tr2 = tr.replace('>', '\m>')
+tr2s = tr2.split('\m')
+tr3 = set(tr2s)
+
+# <codecell>
+
+tr4 = ','.join(tr3).replace(',','')
+
+# <codecell>
+
+print (
+len(tr), 
+len(tr4),
+len(tr)-len(tr4)
+)
+
+# <codecell>
+
+z = open('/Volumes/group_dv/group/data/sequences/transcriptome/Nfur_transcriptome_reduced.fa', 'w')
+z.write(tr4)
+z.close()
+
+# <codecell>
+
+
