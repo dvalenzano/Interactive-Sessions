540bfdfe7c714a74f87b522eeeabb3e3b392f8ff
dario valenzano <drvalenzano@gmail.com>
2014-01-29 18:21:05 +0100

    Update transcriptome_compression.py

--- transcriptome_compression.py
+++ transcriptome_compression.py
@@ -9,7 +9,7 @@ tr = open('/Volumes/group_dv/group/data/sequences/transcriptome/Annotated_Refere
 
 tr2 = tr.replace('>', '\m>')
 tr2s = tr2.split('\m')
-tr3 = set(tr2s)
+tr3 = list(set(tr2s))
 
 # <codecell>
 
@@ -25,34 +25,8 @@ len(tr)-len(tr4)
 
 # <codecell>
 
-#z = open('/Volumes/group_dv/group/data/sequences/transcriptome/Nfur_transcriptome_reduced.fa', 'w')
-#z.write(tr4)
-#z.close()
-
-# <codecell>
-
-keys = tr.split('\n')[:-1][::2]
-values = tr.split('\n')[:-1][1::2]
-d = dict(zip(keys, values))
-
-# <codecell>
-
-keyss = set(keys)
-
-# <codecell>
-
-len(keys)==len(keyss)
-
-# <codecell>
-
-ls = []
-for i in keyss:
-    ls.append(i+'\n'+d[i]+'\n')
-lsj = ','.join(ls).replace('\n,','\n').replace(',','')
-
-# <codecell>
-
 z = open('/Volumes/group_dv/group/data/sequences/transcriptome/Nfur_transcriptome_reduced.fa', 'w')
-z.write(lsj)
+z.write(tr4)
 z.close()
 
+# <codecell>
