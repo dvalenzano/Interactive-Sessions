650e6ac0febfbe6c0754d3f4ab6f52afcfd91ccd
dario valenzano <drvalenzano@gmail.com>
2014-01-30 19:28:58 +0100

    Update ped-to-OneMap.py

--- ped-to-OneMap.py
+++ ped-to-OneMap.py
@@ -3,7 +3,7 @@
 
 # <codecell>
 
-f7 = open('/Volumes/group_dv/personal/DValenzano/Jan2014/F1_inference/inferrednew_fam_7.csv', 'rU').read()
+f7 = open('/Volumes/group_dv/personal/DValenzano/Jan2014/F1_inference/Go_families/inf-fam_7.csv', 'rU').read()
 
 # <codecell>
 
@@ -46,11 +46,7 @@ lw = [i.split(',')[1] for i in lz2.split('\n')[:-1]]
 
 # <codecell>
 
-# I need to correct 
-# 'aa-bb'
-# 'bb-aa'
-# 'ab-bb'
-
+# first I'll check the F1 genotypes
 lx = []
 for i in lw:
     if i not in lx:
@@ -62,95 +58,101 @@ lx
 
 # <codecell>
 
-bbabf1 = []
-for i in lz2.split('\n')[:-1]:
-    if i.split(',')[1]== 'bb-ab':
-        bbabf1.append(i)
-bbabf1 = [i.split(',')[0] for i in bbabf1 ]
-
-bbaaf1 = []
-for i in lz2.split('\n')[:-1]:
-    if i.split(',')[1]== 'bb-aa':
-        bbaaf1.append(i)
-bbaaf1 = [i.split(',')[0] for i in bbaaf1 ]
-
-abbbf1 = []
-for i in lz2.split('\n')[:-1]:
-    if i.split(',')[1]== 'ab-bb':
-        abbbf1.append(i)
-abbbf1 = [i.split(',')[0] for i in abbbf1 ]
-
-aabbf1 = []
-for i in lz2.split('\n')[:-1]:
-    if i.split(',')[1]== 'aa-bb':
-        aabbf1.append(i)
-aabbf1 = [i.split(',')[0] for i in aabbf1 ]
-
-ab0f1 = []
-for i in lz2.split('\n')[:-1]:
-    if i.split(',')[1]== 'ab-0':
-        ab0f1.append(i)
-ab0f1 = [i.split(',')[0] for i in ab0f1 ]
-
-aa0f1 = []
-for i in lz2.split('\n')[:-1]:
-    if i.split(',')[1]== 'aa0':
-        aa0f1.append(i)
-aa0f1 = [i.split(',')[0] for i in aa0f1 ]
-
-oof1 = []
-for i in lz2.split('\n')[:-1]:
-    if i.split(',')[1]== '0-0':
-        oof1.append(i)
-oof1 = [i.split(',')[0] for i in oof1 ]
-
-ac0f1 = []
-for i in lz2.split('\n')[:-1]:
-    if i.split(',')[1]== 'ac-0':
-        ac0f1.append(i)
-ac0f1 = [i.split(',')[0] for i in ac0f1 ]
-
-bb0f1 = []
-for i in lz2.split('\n')[:-1]:
-    if i.split(',')[1]== 'bb-0':
-        bb0f1.append(i)
-bb0f1 = [i.split(',')[0] for i in bb0f1 ]
-
-weird = aabbf1+abbbf1+bbaaf1+bbabf1+ab0f1+aa0f1+oof1+bb0f1
+# Genotype table:
+d = {}
+d['ab-ab'] = 'B3.7'
+d['aa-ab'] = 'D2.15'
+d['ab-aa'] = 'D1.10'
 
 # <codecell>
 
-len(weird)
+d.items()
 
 # <codecell>
 
-import random
-
-good = [i.split(',')[0] for i in lz if i.split(',')[0] not in weird]
-
-# <codecell>
-
-final = weird + random.sample(good, 1606)
+lz3 = []
+for i in lz2.split('\n')[:-1]:
+    lz3.append(i.split(','))
 
 # <codecell>
 
-len(final)
+lz4 = []
+for i in lz3:
+    lz4.append('*'+i[0]+'\t'+d[i[1]]+'\t'+','.join(i[2:]).replace('0','-')+'\n')
+    
 
 # <codecell>
 
-small = [','.join(i)+'\n' for i in f7s[10:] if list(i)[0] in final]
+lz5 = ','.join(lz4).replace('\n,','\n').replace('aa','a').replace('bb','b')
 
 # <codecell>
 
-Small =   ','.join([','.join(list(i))+'\n' for i in f7s[:11]]+small).replace('\n,','\n')
-Smallt = zip(*[i.split(',') for i in Small.split('\n')[:-1]])
-Smallt2 = ','.join([ ','.join(list(i))+'\n' for i in Smallt]).replace('\n,','\n')
+len(lz5.split('\n')[0].split(','))
 
 # <codecell>
 
-z = open('/Volumes/group_dv/personal/DValenzano/Jan2014/F1_inference/small_fam7.csv', 'w')
-z.write(Smallt2)
+fin = str(len(lz5.split('\n')[0].split(',')))+' '+str(len(lz5.split('\n')[:-1]))+'\n'+lz5
+
+# <codecell>
+
+z = open('/Volumes/group_dv/personal/DValenzano/Jan2014/Stanford_paper/fam_7.Onemap.txt', 'w')
+z.write(fin)
 z.close()
+
+
+#######################################################################################################
+#######################                       ITERATIVELY                       #######################
+#######################################################################################################
+
+
+input = open('/Volumes/group_dv/personal/DValenzano/Jan2014/F1_inference/Go_families/inf-list.txt', 'rU').read() #inferred-list.txt is a file that contains all the paths to the inferred files:
+files = input.split('\n')[:-1]
+for f in files:
+    fam = open(f, 'rU').read()
+    fams = zip(*[ i.split(',') for i in fam.split('\n')[:-2]])
+    # What I want is marker ID, F1 genotypes, F2 genotypes
+    ls = []
+    for i in fams[10:]:
+        ls.append([i[0]]+[i[3]+'-'+i[4]]+list(i[5:]))
+    lz = []
+    for i in ls:
+        lz.append(','.join(i)+'\n')
+    lz2 = ','.join(lz).replace('\n,','\n')
+    #d = {}
+    #d['ab-ab'] = 'B2.6'
+    #d['aa-ab'] = 'D2.15'
+    #d['ab-aa'] = 'D1.10'
+    #lz3 = [ i.split(',')[0]+','+d[i.split(',')[1]]+','+','.join(i.split(',')[2:]) +'\n' for i in lz2.split('\n')[:-1]]
+    #lw = [i.split(',')[1] for i in lz2.split('\n')[:-1]]
+    lx = []
+    for i in lw:
+        if i not in lx:
+            lx.append(i)
+    #lx
+    d = {}
+    d['ab-ab'] = 'B3.7'
+    d['aa-ab'] = 'D2.15'
+    d['ab-aa'] = 'D1.10'
+    d.items()
+    lz3 = []
+    for i in lz2.split('\n')[:-1]:
+        lz3.append(i.split(','))
+    lz4 = []
+    for i in lz3:
+        lz4.append('*'+i[0]+'\t'+d[i[1]]+'\t'+','.join(i[2:]).replace('0','-')+'\n')
+    lz5 = ','.join(lz4).replace('\n,','\n').replace('aa','a').replace('bb','b')
+    len(lz5.split('\n')[0].split(','))
+    fin = str(len(lz5.split('\n')[0].split(',')))+' '+str(len(lz5.split('\n')[:-1]))+'\n'+lz5
+    o = '/Volumes/group_dv/personal/DValenzano/Jan2014/Stanford_paper/OneMap/'+ ','.join(f.split('-')[1].split('.')[:-1]).replace(',','.')+ '_OneMap.txt'
+    z = open(o, 'w')
+    z.write(fin)
+    z.close()
+
+
+# <codecell>
+
+f = '/Volumes/group_dv/personal/DValenzano/Jan2014/F1_inference/Go_families/inf-fam_7.1.csv'
+'/Volumes/group_dv/personal/DValenzano/Jan2014/Stanford_paper/OneMap/'+ ','.join(f.split('-')[1].split('.')[:-1]).replace(',','.')+ '_OneMap.txt'
 
 # <codecell>
 
