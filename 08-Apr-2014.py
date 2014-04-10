8a21a025a7b7796cf9f47036cf1c1b966866c296
dario valenzano <drvalenzano@gmail.com>
2014-04-10 19:16:17 +0200

    Update 08-Apr-2014.py

--- 08-Apr-2014.py
+++ 08-Apr-2014.py
@@ -44,3 +44,25 @@ z = open(out, 'w')
 z.write(om_sig1)
 z.close()
 
+
+########################################################################################################################
+########################################        Below the latest version        ########################################      
+########################################################################################################################
+
+
+gender = raw_input('What gender are you interested in processing [fem, mal]?\n')
+gender_init = gender[0]
+family = raw_input('What family are you interested in processing?\n')
+omin = '/Volumes/group_dv/personal/DValenzano/Apr2014/plink/fam_%s/%s/fam_%s%s_OneMap.txt' %(family, gender, family, gender_init)
+om = open(omin, 'rU').read()
+assig = '/Volumes/group_dv/personal/DValenzano/Apr2014/plink/fam_%s/%s/fam_%s%s_sqtl_sig' %(family, gender, family, gender_init)
+sig = open(assig, 'rU').read()
+s = ','.join([ i.split()[1]+','+i.split()[-1]+'\n'  for i in sig.split('\n')[:-1] ]).replace('\n,','\n')
+sigmark = [ i.split(',')[0] for i in s.split('\n')[1:-1]]
+om_sig0 = ','.join([ i+'\n'  for i in om.split('\n')[:-1] if i.split('\t')[0][1:] in sigmark ]).replace('\n,','\n')
+om_sig1 = str(len(om_sig0.split('\n')[0].split('\t')[2].split(',')))+' '+str(len(om_sig0.split('\n')))+'\n'+om_sig0
+out = '/Volumes/group_dv/personal/DValenzano/Apr2014/plink/fam_%s/%s/fam_%s%s_omsig' %(family, gender, family, gender_init)
+z = open(out, 'w')
+z.write(om_sig1)
+z.close()
+
