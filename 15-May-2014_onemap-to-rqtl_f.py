da45ef4e31953465be59a4efbeec2c04f36ddced
dario valenzano <drvalenzano@gmail.com>
2014-05-15 11:49:11 +0200

    Create 15-May-2014_onemap-to-rqtl_f.py

---
+++ 15-May-2014_onemap-to-rqtl_f.py
@@ -0,0 +1,199 @@
+
+f7flg1_0 = open('/Volumes/group_dv/personal/DValenzano/Apr2014/R/f7f/LG1', 'rU').read()
+f7flg1 = ','.join([ '1,'+i+'\n'  for i in f7flg1_0.split('\n')  ]).replace('\n,','\n')[:-1]
+f7flg2_0 = open('/Volumes/group_dv/personal/DValenzano/Apr2014/R/f7f/LG2', 'rU').read()
+f7flg2 = ','.join([ '2,'+i+'\n'  for i in f7flg2_0.split('\n')  ]).replace('\n,','\n')[:-1]
+f7flg3_0 = open('/Volumes/group_dv/personal/DValenzano/Apr2014/R/f7f/LG3', 'rU').read()
+f7flg3 = ','.join([ '3,'+i+'\n'  for i in f7flg3_0.split('\n')  ]).replace('\n,','\n')[:-1]
+f7flg4_0 = open('/Volumes/group_dv/personal/DValenzano/Apr2014/R/f7f/LG4', 'rU').read()
+f7flg4 = ','.join([ '4,'+i+'\n'  for i in f7flg4_0.split('\n')  ]).replace('\n,','\n')[:-1]
+f7flg5_0 = open('/Volumes/group_dv/personal/DValenzano/Apr2014/R/f7f/LG5', 'rU').read()
+f7flg5 = ','.join([ '5,'+i+'\n'  for i in f7flg5_0.split('\n')  ]).replace('\n,','\n')[:-1]
+f7flg6_0 = open('/Volumes/group_dv/personal/DValenzano/Apr2014/R/f7f/LG6', 'rU').read()
+f7flg6 = ','.join([ '6,'+i+'\n'  for i in f7flg6_0.split('\n')  ]).replace('\n,','\n')[:-1]
+f7flg7_0 = open('/Volumes/group_dv/personal/DValenzano/Apr2014/R/f7f/LG7', 'rU').read()
+f7flg7 = ','.join([ '7,'+i+'\n'  for i in f7flg7_0.split('\n')  ]).replace('\n,','\n')[:-1]
+f7flg8_0 = open('/Volumes/group_dv/personal/DValenzano/Apr2014/R/f7f/LG8', 'rU').read()
+f7flg8 = ','.join([ '8,'+i+'\n'  for i in f7flg8_0.split('\n')  ]).replace('\n,','\n')[:-1]
+f7flg9_0 = open('/Volumes/group_dv/personal/DValenzano/Apr2014/R/f7f/LG9', 'rU').read()
+f7flg9 = ','.join([ '9,'+i+'\n'  for i in f7flg9_0.split('\n')  ]).replace('\n,','\n')[:-1]
+f7flg10_0 = open('/Volumes/group_dv/personal/DValenzano/Apr2014/R/f7f/LG10', 'rU').read()
+f7flg10 = ','.join([ '10,'+i+'\n'  for i in f7flg10_0.split('\n')  ]).replace('\n,','\n')[:-1]
+f7flg11_0 = open('/Volumes/group_dv/personal/DValenzano/Apr2014/R/f7f/LG11', 'rU').read()
+f7flg11 = ','.join([ '11,'+i+'\n'  for i in f7flg11_0.split('\n')  ]).replace('\n,','\n')[:-1]
+f7flg12_0 = open('/Volumes/group_dv/personal/DValenzano/Apr2014/R/f7f/LG12', 'rU').read()
+f7flg12 = ','.join([ '12,'+i+'\n'  for i in f7flg12_0.split('\n')  ]).replace('\n,','\n')[:-1]
+f7flg13_0 = open('/Volumes/group_dv/personal/DValenzano/Apr2014/R/f7f/LG13', 'rU').read()
+f7flg13 = ','.join([ '13,'+i+'\n'  for i in f7flg13_0.split('\n')  ]).replace('\n,','\n')[:-1]
+f7flg14_0 = open('/Volumes/group_dv/personal/DValenzano/Apr2014/R/f7f/LG14', 'rU').read()
+f7flg14 = ','.join([ '14,'+i+'\n'  for i in f7flg14_0.split('\n')  ]).replace('\n,','\n')[:-1]
+f7flg15_0 = open('/Volumes/group_dv/personal/DValenzano/Apr2014/R/f7f/LG15', 'rU').read()
+f7flg15 = ','.join([ '15,'+i+'\n'  for i in f7flg15_0.split('\n')  ]).replace('\n,','\n')[:-1]
+f7flg16_0 = open('/Volumes/group_dv/personal/DValenzano/Apr2014/R/f7f/LG16', 'rU').read()
+f7flg16 = ','.join([ '16,'+i+'\n'  for i in f7flg16_0.split('\n')  ]).replace('\n,','\n')[:-1]
+f7flg17_0 = open('/Volumes/group_dv/personal/DValenzano/Apr2014/R/f7f/LG17', 'rU').read()
+f7flg17 = ','.join([ '17,'+i+'\n'  for i in f7flg17_0.split('\n')  ]).replace('\n,','\n')[:-1]
+f7flg18_0 = open('/Volumes/group_dv/personal/DValenzano/Apr2014/R/f7f/LG18', 'rU').read()
+f7flg18 = ','.join([ '18,'+i+'\n'  for i in f7flg18_0.split('\n')  ]).replace('\n,','\n')[:-1]
+f7flg19_0 = open('/Volumes/group_dv/personal/DValenzano/Apr2014/R/f7f/LG19', 'rU').read()
+f7flg19 = ','.join([ '19,'+i+'\n'  for i in f7flg19_0.split('\n')  ]).replace('\n,','\n')[:-1]
+f7flg20_0 = open('/Volumes/group_dv/personal/DValenzano/Apr2014/R/f7f/LG20', 'rU').read()
+f7flg20 = ','.join([ '20,'+i+'\n'  for i in f7flg20_0.split('\n')  ]).replace('\n,','\n')[:-1]
+f7flg21_0 = open('/Volumes/group_dv/personal/DValenzano/Apr2014/R/f7f/LG21', 'rU').read()
+f7flg21 = ','.join([ '21,'+i+'\n'  for i in f7flg21_0.split('\n')  ]).replace('\n,','\n')[:-1]
+f7flg22_0 = open('/Volumes/group_dv/personal/DValenzano/Apr2014/R/f7f/LG22', 'rU').read()
+f7flg22 = ','.join([ '22,'+i+'\n'  for i in f7flg22_0.split('\n')  ]).replace('\n,','\n')[:-1]
+f7flg23_0 = open('/Volumes/group_dv/personal/DValenzano/Apr2014/R/f7f/LG23', 'rU').read()
+f7flg23 = ','.join([ '23,'+i+'\n'  for i in f7flg23_0.split('\n')  ]).replace('\n,','\n')[:-1]
+f7flg24_0 = open('/Volumes/group_dv/personal/DValenzano/Apr2014/R/f7f/LG24', 'rU').read()
+f7flg24 = ','.join([ '24,'+i+'\n'  for i in f7flg24_0.split('\n')  ]).replace('\n,','\n')[:-1]
+f7flg25_0 = open('/Volumes/group_dv/personal/DValenzano/Apr2014/R/f7f/LG25', 'rU').read()
+f7flg25 = ','.join([ '25,'+i+'\n'  for i in f7flg25_0.split('\n')  ]).replace('\n,','\n')[:-1]
+f7flg26_0 = open('/Volumes/group_dv/personal/DValenzano/Apr2014/R/f7f/LG26', 'rU').read()
+f7flg26 = ','.join([ '26,'+i+'\n'  for i in f7flg26_0.split('\n')  ]).replace('\n,','\n')[:-1]
+
+# <codecell>
+f8flg1_0 = open('/Volumes/group_dv/personal/DValenzano/Apr2014/R/f8f/LG1', 'rU').read()
+f8flg1 = ','.join([ '1,'+i+'\n'  for i in f8flg1_0.split('\n')  ]).replace('\n,','\n')[:-1]
+f8flg2_0 = open('/Volumes/group_dv/personal/DValenzano/Apr2014/R/f8f/LG2', 'rU').read()
+f8flg2 = ','.join([ '2,'+i+'\n'  for i in f8flg2_0.split('\n')  ]).replace('\n,','\n')[:-1]
+f8flg3_0 = open('/Volumes/group_dv/personal/DValenzano/Apr2014/R/f8f/LG3', 'rU').read()
+f8flg3 = ','.join([ '3,'+i+'\n'  for i in f8flg3_0.split('\n')  ]).replace('\n,','\n')[:-1]
+f8flg4_0 = open('/Volumes/group_dv/personal/DValenzano/Apr2014/R/f8f/LG4', 'rU').read()
+f8flg4 = ','.join([ '4,'+i+'\n'  for i in f8flg4_0.split('\n')  ]).replace('\n,','\n')[:-1]
+f8flg5_0 = open('/Volumes/group_dv/personal/DValenzano/Apr2014/R/f8f/LG5', 'rU').read()
+f8flg5 = ','.join([ '5,'+i+'\n'  for i in f8flg5_0.split('\n')  ]).replace('\n,','\n')[:-1]
+f8flg6_0 = open('/Volumes/group_dv/personal/DValenzano/Apr2014/R/f8f/LG6', 'rU').read()
+f8flg6 = ','.join([ '6,'+i+'\n'  for i in f8flg6_0.split('\n')  ]).replace('\n,','\n')[:-1]
+f8flg7_0 = open('/Volumes/group_dv/personal/DValenzano/Apr2014/R/f8f/LG7', 'rU').read()
+f8flg7 = ','.join([ '7,'+i+'\n'  for i in f8flg7_0.split('\n')  ]).replace('\n,','\n')[:-1]
+f8flg8_0 = open('/Volumes/group_dv/personal/DValenzano/Apr2014/R/f8f/LG8', 'rU').read()
+f8flg8 = ','.join([ '8,'+i+'\n'  for i in f8flg8_0.split('\n')  ]).replace('\n,','\n')[:-1]
+f8flg9_0 = open('/Volumes/group_dv/personal/DValenzano/Apr2014/R/f8f/LG9', 'rU').read()
+f8flg9 = ','.join([ '9,'+i+'\n'  for i in f8flg9_0.split('\n')  ]).replace('\n,','\n')[:-1]
+f8flg10_0 = open('/Volumes/group_dv/personal/DValenzano/Apr2014/R/f8f/LG10', 'rU').read()
+f8flg10 = ','.join([ '10,'+i+'\n'  for i in f8flg10_0.split('\n')  ]).replace('\n,','\n')[:-1]
+f8flg11_0 = open('/Volumes/group_dv/personal/DValenzano/Apr2014/R/f8f/LG11', 'rU').read()
+f8flg11 = ','.join([ '11,'+i+'\n'  for i in f8flg11_0.split('\n')  ]).replace('\n,','\n')[:-1]
+f8flg12_0 = open('/Volumes/group_dv/personal/DValenzano/Apr2014/R/f8f/LG12', 'rU').read()
+f8flg12 = ','.join([ '12,'+i+'\n'  for i in f8flg12_0.split('\n')  ]).replace('\n,','\n')[:-1]
+f8flg13_0 = open('/Volumes/group_dv/personal/DValenzano/Apr2014/R/f8f/LG13', 'rU').read()
+f8flg13 = ','.join([ '13,'+i+'\n'  for i in f8flg13_0.split('\n')  ]).replace('\n,','\n')[:-1]
+
+# <codecell>
+f14flg1_0 = open('/Volumes/group_dv/personal/DValenzano/Apr2014/R/f14f/LG1', 'rU').read()
+f14flg1 = ','.join([ '1,'+i+'\n'  for i in f14flg1_0.split('\n')  ]).replace('\n,','\n')[:-1]
+f14flg2_0 = open('/Volumes/group_dv/personal/DValenzano/Apr2014/R/f14f/LG2', 'rU').read()
+f14flg2 = ','.join([ '2,'+i+'\n'  for i in f14flg2_0.split('\n')  ]).replace('\n,','\n')[:-1]
+f14flg3_0 = open('/Volumes/group_dv/personal/DValenzano/Apr2014/R/f14f/LG3', 'rU').read()
+f14flg3 = ','.join([ '3,'+i+'\n'  for i in f14flg3_0.split('\n')  ]).replace('\n,','\n')[:-1]
+f14flg4_0 = open('/Volumes/group_dv/personal/DValenzano/Apr2014/R/f14f/LG4', 'rU').read()
+f14flg4 = ','.join([ '4,'+i+'\n'  for i in f14flg4_0.split('\n')  ]).replace('\n,','\n')[:-1]
+f14flg5_0 = open('/Volumes/group_dv/personal/DValenzano/Apr2014/R/f14f/LG5', 'rU').read()
+f14flg5 = ','.join([ '5,'+i+'\n'  for i in f14flg5_0.split('\n')  ]).replace('\n,','\n')[:-1]
+f14flg6_0 = open('/Volumes/group_dv/personal/DValenzano/Apr2014/R/f14f/LG6', 'rU').read()
+f14flg6 = ','.join([ '6,'+i+'\n'  for i in f14flg6_0.split('\n')  ]).replace('\n,','\n')[:-1]
+f14flg7_0 = open('/Volumes/group_dv/personal/DValenzano/Apr2014/R/f14f/LG7', 'rU').read()
+f14flg7 = ','.join([ '7,'+i+'\n'  for i in f14flg7_0.split('\n')  ]).replace('\n,','\n')[:-1]
+f14flg8_0 = open('/Volumes/group_dv/personal/DValenzano/Apr2014/R/f14f/LG8', 'rU').read()
+f14flg8 = ','.join([ '8,'+i+'\n'  for i in f14flg8_0.split('\n')  ]).replace('\n,','\n')[:-1]
+f14flg9_0 = open('/Volumes/group_dv/personal/DValenzano/Apr2014/R/f14f/LG9', 'rU').read()
+f14flg9 = ','.join([ '9,'+i+'\n'  for i in f14flg9_0.split('\n')  ]).replace('\n,','\n')[:-1]
+f14flg10_0 = open('/Volumes/group_dv/personal/DValenzano/Apr2014/R/f14f/LG10', 'rU').read()
+f14flg10 = ','.join([ '10,'+i+'\n'  for i in f14flg10_0.split('\n')  ]).replace('\n,','\n')[:-1]
+f14flg11_0 = open('/Volumes/group_dv/personal/DValenzano/Apr2014/R/f14f/LG11', 'rU').read()
+f14flg11 = ','.join([ '11,'+i+'\n'  for i in f14flg11_0.split('\n')  ]).replace('\n,','\n')[:-1]
+f14flg12_0 = open('/Volumes/group_dv/personal/DValenzano/Apr2014/R/f14f/LG12', 'rU').read()
+f14flg12 = ','.join([ '12,'+i+'\n'  for i in f14flg12_0.split('\n')  ]).replace('\n,','\n')[:-1]
+f14flg13_0 = open('/Volumes/group_dv/personal/DValenzano/Apr2014/R/f14f/LG13', 'rU').read()
+f14flg13 = ','.join([ '13,'+i+'\n'  for i in f14flg13_0.split('\n')  ]).replace('\n,','\n')[:-1]
+f14flg14_0 = open('/Volumes/group_dv/personal/DValenzano/Apr2014/R/f14f/LG14', 'rU').read()
+f14flg14 = ','.join([ '14,'+i+'\n'  for i in f14flg14_0.split('\n')  ]).replace('\n,','\n')[:-1]
+f14flg15_0 = open('/Volumes/group_dv/personal/DValenzano/Apr2014/R/f14f/LG15', 'rU').read()
+f14flg15 = ','.join([ '15,'+i+'\n'  for i in f14flg15_0.split('\n')  ]).replace('\n,','\n')[:-1]
+f14flg16_0 = open('/Volumes/group_dv/personal/DValenzano/Apr2014/R/f14f/LG16', 'rU').read()
+f14flg16 = ','.join([ '16,'+i+'\n'  for i in f14flg16_0.split('\n')  ]).replace('\n,','\n')[:-1]
+f14flg17_0 = open('/Volumes/group_dv/personal/DValenzano/Apr2014/R/f14f/LG17', 'rU').read()
+f14flg17 = ','.join([ '17,'+i+'\n'  for i in f14flg17_0.split('\n')  ]).replace('\n,','\n')[:-1]
+f14flg18_0 = open('/Volumes/group_dv/personal/DValenzano/Apr2014/R/f14f/LG18', 'rU').read()
+f14flg18 = ','.join([ '18,'+i+'\n'  for i in f14flg18_0.split('\n')  ]).replace('\n,','\n')[:-1]
+f14flg19_0 = open('/Volumes/group_dv/personal/DValenzano/Apr2014/R/f14f/LG19', 'rU').read()
+f14flg19 = ','.join([ '19,'+i+'\n'  for i in f14flg19_0.split('\n')  ]).replace('\n,','\n')[:-1]
+f14flg20_0 = open('/Volumes/group_dv/personal/DValenzano/Apr2014/R/f14f/LG20', 'rU').read()
+f14flg20 = ','.join([ '20,'+i+'\n'  for i in f14flg20_0.split('\n')  ]).replace('\n,','\n')[:-1]
+f14flg21_0 = open('/Volumes/group_dv/personal/DValenzano/Apr2014/R/f14f/LG21', 'rU').read()
+f14flg21 = ','.join([ '21,'+i+'\n'  for i in f14flg21_0.split('\n')  ]).replace('\n,','\n')[:-1]
+f14flg22_0 = open('/Volumes/group_dv/personal/DValenzano/Apr2014/R/f14f/LG22', 'rU').read()
+f14flg22 = ','.join([ '22,'+i+'\n'  for i in f14flg22_0.split('\n')  ]).replace('\n,','\n')[:-1]
+f14flg23_0 = open('/Volumes/group_dv/personal/DValenzano/Apr2014/R/f14f/LG23', 'rU').read()
+f14flg23 = ','.join([ '23,'+i+'\n'  for i in f14flg23_0.split('\n')  ]).replace('\n,','\n')[:-1]
+f14flg24_0 = open('/Volumes/group_dv/personal/DValenzano/Apr2014/R/f14f/LG24', 'rU').read()
+f14flg24 = ','.join([ '24,'+i+'\n'  for i in f14flg24_0.split('\n')  ]).replace('\n,','\n')[:-1]
+f14flg25_0 = open('/Volumes/group_dv/personal/DValenzano/Apr2014/R/f14f/LG25', 'rU').read()
+f14flg25 = ','.join([ '25,'+i+'\n'  for i in f14flg25_0.split('\n')  ]).replace('\n,','\n')[:-1]
+f14flg26_0 = open('/Volumes/group_dv/personal/DValenzano/Apr2014/R/f14f/LG26', 'rU').read()
+f14flg26 = ','.join([ '26,'+i+'\n'  for i in f14flg26_0.split('\n')  ]).replace('\n,','\n')[:-1]
+
+
+# <codecell>
+
+f14f = f14flg1+'\n\n'+f14flg2+'\n\n'+f14flg3+'\n\n'+f14flg4+'\n\n'+f14flg5+'\n\n'+f14flg6+'\n\n'+f14flg7+'\n\n'+f14flg8+'\n\n'+f14flg9+'\n\n'+f14flg10+'\n\n'+f14flg11+'\n\n'+f14flg12+'\n\n'+f14flg13+'\n\n'+f14flg14+'\n\n'+f14flg15+'\n\n'+f14flg16+'\n\n'+f14flg17+'\n\n'+f14flg18+'\n\n'+f14flg19+'\n\n'+f14flg20+'\n\n'+f14flg21+'\n\n'+f14flg22+'\n\n'+f14flg23+'\n\n'+f14flg24+'\n\n'+f14flg25+'\n\n'+f14flg26
+f7f = f7flg1+'\n\n'+f7flg2+'\n\n'+f7flg3+'\n\n'+f7flg4+'\n\n'+f7flg5+'\n\n'+f7flg6+'\n\n'+f7flg7+'\n\n'+f7flg8+'\n\n'+f7flg9+'\n\n'+f7flg10+'\n\n'+f7flg11+'\n\n'+f7flg12+'\n\n'+f7flg13+'\n\n'+f7flg14+'\n\n'+f7flg15+'\n\n'+f7flg16+'\n\n'+f7flg17+'\n\n'+f7flg18+'\n\n'+f7flg19+'\n\n'+f7flg20+'\n\n'+f7flg21+'\n\n'+f7flg22+'\n\n'+f7flg23+'\n\n'+f7flg24+'\n\n'+f7flg25+'\n\n'+f7flg26
+f8f = f8flg1+'\n\n'+f8flg2+'\n\n'+f8flg3+'\n\n'+f8flg4+'\n\n'+f8flg5+'\n\n'+f8flg6+'\n\n'+f8flg7+'\n\n'+f8flg8+'\n\n'+f8flg9+'\n\n'+f8flg10+'\n\n'+f8flg11+'\n\n'+f8flg12+'\n\n'+f8flg13
+
+
+# <codecell>
+
+Golg = open('/Volumes/group_dv/personal/DValenzano/Apr2014/R/go32014_all_pos.csv', 'rU').read()
+
+# <codecell>
+
+golgs = Golg.split('\n')[:-1]
+ks = [i.split(',')[0].replace('"','') for i in golgs]
+vls = [i.replace('"','') for i in golgs]
+d = dict(zip(ks, vls))
+
+# <codecell>
+
+ls7 = []
+for i in f7f.split('\n\n'):
+    for j in i.split('\n'):
+        if j.split(',')[1] in ks:
+            ls7.append(j+','+d[j.split(',')[1]]+'\n')
+        else:
+            ls7.append(j+',,,\n')
+            
+ls8 = []
+for i in f8f.split('\n\n'):
+    for j in i.split('\n'):
+        if j.split(',')[1] in ks:
+            ls8.append(j+','+d[j.split(',')[1]]+'\n')
+        else:
+            ls8.append(j+',,,\n')
+
+            
+ls14 = []
+for i in f14f.split('\n\n'):
+    for j in i.split('\n'):
+        if j.split(',')[1] in ks:
+            ls14.append(j+','+d[j.split(',')[1]]+'\n')
+        else:
+            ls14.append(j+',,,\n')
+
+# <codecell>
+
+ls7j = 'LGom,marker,poson,p-val,mar,LGrqtl,posrqtl\n'+','.join(ls7).replace('\n,','\n')
+ls14j = 'LGom,marker,poson,p-val,mar,LGrqtl,posrqtl\n'+','.join(ls14).replace('\n,','\n')
+ls8j = 'LGom,marker,poson,p-val,mar,LGrqtl,posrqtl\n'+','.join(ls8).replace('\n,','\n')
+
+# <codecell>
+
+z = open('/Volumes/group_dv/personal/DValenzano/Apr2014/R/f7f/f7f_om-rqtl.csv', 'w')
+x = open('/Volumes/group_dv/personal/DValenzano/Apr2014/R/f14f/f14f_om-rqtl.csv', 'w')
+y = open('/Volumes/group_dv/personal/DValenzano/Apr2014/R/f8f/f8f_om-rqtl.csv', 'w')
+z.write(ls7j)
+x.write(ls14j)
+y.write(ls8j)
+z.close()
+x.close()
+y.close()
