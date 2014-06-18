4ce0bef5bd8245b375ca881e06599c75da29bfd9
dario valenzano <drvalenzano@gmail.com>
2014-06-18 17:14:36 +0200

    Update 06-Apr-2014.py

--- 06-Apr-2014.py
+++ 06-Apr-2014.py
@@ -81,14 +81,16 @@ z.close()
 
 # <codecell>
 
-tab8mas = open('/Volumes/group_dv/personal/DValenzano/Apr2014/plink/fam_14/mal/fam_14m_sqtl.assoc.linear', 'rU').read()
+#tab8mas = open('/Volumes/group_dv/personal/DValenzano/Apr2014/plink/fam_14/mal/fam_14m_sqtl.assoc.linear', 'rU').read()
+tab8mas = open('/Volumes/group_dv/personal/DValenzano/Apr2014/plink/fam_8/mal/fam_8m_sqtl.assoc.linear', 'rU').read()
 low1 = [ i+'\n' for i in tab8mas.split('\n')[1:-1] if i.split()[-1] != 'NA' and float(i.split()[-1]) < 0.05]
 lowz = tab8mas.split('\n')[0] + '\n'+','.join(low1).replace('\n,','\n')
 z = open('/Volumes/group_dv/personal/DValenzano/Apr2014/plink/fam_8/mal/fam_8m_sqtl_sig', 'w')
 z.write(lowz)
 z.close()
 
-tab8fas = open('/Volumes/group_dv/personal/DValenzano/Apr2014/plink/fam_14/fem/fam_14f_sqtl.assoc.linear', 'rU').read()
+#tab8fas = open('/Volumes/group_dv/personal/DValenzano/Apr2014/plink/fam_14/fem/fam_14f_sqtl.assoc.linear', 'rU').read()
+tab8fas = open('/Volumes/group_dv/personal/DValenzano/Apr2014/plink/fam_8/fem/fam_8f_sqtl.assoc.linear', 'rU').read()
 low1 = [ i+'\n' for i in tab8fas.split('\n')[1:-1] if i.split()[-1] != 'NA' and float(i.split()[-1]) < 0.05]
 lowz = tab8fas.split('\n')[0] + '\n'+','.join(low1).replace('\n,','\n')
 z = open('/Volumes/group_dv/personal/DValenzano/Apr2014/plink/fam_8/fem/fam_8f_sqtl_sig', 'w')
