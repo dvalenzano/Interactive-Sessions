2d471fb730e91e5422cea24096b3a9de3e001f25
dario valenzano <drvalenzano@gmail.com>
2014-06-23 19:23:43 +0200

    Update 18-Jun-2014.py

--- 18-Jun-2014.py
+++ 18-Jun-2014.py
@@ -468,6 +468,140 @@ print(
 )
 
 
+# Now I first need to generate a list of all the markers for crosses G and AA 
+
+# In[231]:
+
+lgall = list(Set(g7m_l+ge7m_l+g7f_l+ge7f_l+g14m_l+ge14m_l+g14f_l+ge14f_l+g8m_l+ge8m_l+g8f_l+ge8f_l+g1_1m_l+g1_1f_l))
+laaall = list(Set(aa1m_l+aae1m_l+aa1f_l+aae1f_l+aa3m_l+aae3m_l))
+
+
+# In[114]:
+
+gh = 'M_ID,g7m_l,ge7m_l,g7f_l,ge7f_l,g14m_l,ge14m_l,g14f_l,ge14f_l,g8m_l,ge8m_l,g8f_l,ge8f_l,g1_1m_l,g1_1f_l\n'
+aah = 'M_ID,aa1m_l,aae1m_l,aa1f_l,aae1f_l,aa3m_l,aae3m_l\n'
+
+
+# In[234]:
+
+gm7_c = ['1' if i in g7m_l else '0' for i in lgall ]
+gm14_c = ['1' if i in g14m_l else '0' for i in lgall ]
+gm8_c = ['1' if i in g8m_l else '0' for i in lgall ]
+gm1_1_c = ['1' if i in g1_1m_l else '0' for i in lgall ]
+gf7_c = ['1' if i in g7f_l else '0' for i in lgall ]
+gf14_c = ['1' if i in g14f_l else '0' for i in lgall ]
+gf8_c = ['1' if i in g8f_l else '0' for i in lgall ]
+gf1_1_c = ['1' if i in g1_1f_l else '0' for i in lgall ]
+ge7m_c = ['1' if i in ge7m_l else '0' for i in lgall ]
+ge7f_c = ['1' if i in ge7f_l else '0' for i in lgall ]
+ge14m_c = ['1' if i in ge14m_l else '0' for i in lgall ]
+ge14f_c = ['1' if i in ge14f_l else '0' for i in lgall ]
+ge8m_c = ['1' if i in ge8m_l else '0' for i in lgall ]
+ge8f_c = ['1' if i in ge8f_l else '0' for i in lgall ]
+
+aa1m_c = ['1' if i in aa1m_l else '0' for i in laaall ]
+aa1f_c = ['1' if i in aa1f_l else '0' for i in laaall ]
+aa3m_c = ['1' if i in aa3m_l else '0' for i in laaall ]
+aae1m_c = ['1' if i in aae1m_l else '0' for i in laaall ]
+aae1f_c = ['1' if i in aae1f_l else '0' for i in laaall ]
+aae3m_c = ['1' if i in aae3m_l else '0' for i in laaall ]
+
+
+# In[139]:
+
+#gtab =  [list(i) for i in zip(*[[gm7_c]+[ge7m_c]+[gf7_c]+[ge7f_c]+[gm14_c]+[ge14m_c]+[gf14_c]+[ge14f_c]+[gm8_c]+[ge8m_c]+[gf8_c]+[ge8f_c]+[gm1_1_c]+[gf1_1_c]])]
+
+
+# In[151]:
+
+gtab =  [list(i) for i in zip(*(gm7_c,ge7m_c,gf7_c,ge7f_c,gm14_c,ge14m_c,gf14_c,ge14f_c,gm8_c,ge8m_c,gf8_c,ge8f_c,gm1_1_c,gf1_1_c))]
+
+
+# In[152]:
+
+gtabj = [','.join(i)+'\n' for i in gtab]
+
+
+# In[158]:
+
+gtabj_2 = gh+','.join([lgall[i]+','+gtabj[i] for i in range(len(gtabj))]).replace('\n,','\n')
+
+
+# In[219]:
+
+z = open('/Volumes/group_dv/personal/DValenzano/Jun2014/all_Ggroups.csv', 'w')
+z.write(gtabj_2)
+z.close()
+
+
+# In[165]:
+
+gtabj_2.split('\n')[1].split(',')[1:].count('1')
+
+
+# In[248]:
+
+gcounts = [i.split(',')[1:].count('1') for i in gtabj_2.split('\n')[1:]]
+
+
+# In[249]:
+
+get_ipython().magic(u'pylab inline')
+import math
+import scipy
+import numpy as np
+from numpy.random import randn
+import pandas as pd
+from scipy import stats
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import seaborn as sns
+from __future__ import division
+#import utils
+import seaborn
+seaborn.set()
+colors = seaborn.color_palette()
+
+
+# In[250]:
+
+gca = np.asarray(gcounts)
+
+
+# In[202]:
+
+plt.hist(gca)
+
+
+##### The average number of times a markers is signficant amont the groups is about 1.31
+
+###### Now for cross AA
+
+# In[236]:
+
+aatab =  [list(i) for i in zip(*[aa1m_c,aae1m_c,aa1f_c,aae1f_c,aa3m_c,aae3m_c])]
+
+
+# In[237]:
+
+aatabj = [','.join(i)+'\n' for i in aatab]
+aatabj_2 = aah+','.join([laaall[i]+','+aatabj[i] for i in range(len(aatabj))]).replace('\n,','\n')
+
+
+# In[238]:
+
+z = open('/Volumes/group_dv/personal/DValenzano/Jun2014/all_AAgroups.csv', 'w')
+z.write(aatabj_2)
+z.close()
+
+
+# In[268]:
+
+aacounts = [i.split(',')[1:].count('1') for i in aatabj_2.split('\n')[1:]]
+aaca = np.asarray(aacounts)
+plt.hist(aaca)
+
+
 # In[ ]:
 
 
