89e4b83b2f78059a8151c1e3967a15217e4b984a
dario valenzano <drvalenzano@gmail.com>
2014-06-29 17:40:43 +0200

    Update 25-Jun-2014.py

--- 25-Jun-2014.py
+++ 25-Jun-2014.py
@@ -5,7 +5,7 @@
 
 #### Date: 25.06.2014
 
-# In[1]:
+# In[357]:
 
 g = open('/Volumes/group_dv/personal/DValenzano/Jun2014/all_Ggroups.csv', 'rU').read()
 aa = open('/Volumes/group_dv/personal/DValenzano/Jun2014/all_AAgroups.csv', 'rU').read()
@@ -13,7 +13,7 @@ aa = open('/Volumes/group_dv/personal/DValenzano/Jun2014/all_AAgroups.csv', 'rU'
 
 # Now I need to get the p-values for each marker, group by group.
 
-# In[6]:
+# In[358]:
 
 #This is the old way to do it:
 #pv_g7m = open('/Volumes/group_dv/personal/DValenzano/Apr2014/plink/fam_7/mal/fam_7m_sqtl_sig', 'rU').read()
@@ -22,7 +22,7 @@ aa = open('/Volumes/group_dv/personal/DValenzano/Jun2014/all_AAgroups.csv', 'rU'
 #g7md = dict(zip(g7mk, g7mv))
 
 
-# In[130]:
+# In[359]:
 
 g7min = '/Volumes/group_dv/personal/DValenzano/Apr2014/plink/fam_7/mal/fam_7m_sqtl_sig'
 g7fin = '/Volumes/group_dv/personal/DValenzano/Apr2014/plink/fam_7/fem/fam_7f_sqtl_sig'
@@ -34,7 +34,7 @@ g1_1min = '/Volumes/group_dv/personal/DValenzano/Apr2014/plink/fam_1.1/mal/fam_1
 g1_1fin = '/Volumes/group_dv/personal/DValenzano/Apr2014/plink/fam_1.1/fem/fam_1.1f_sqtl_sig'
 
 
-# In[93]:
+# In[360]:
 
 #class Makep3(object):
 #    " generates a dictionary with marker as key and p-value as value"
@@ -49,7 +49,7 @@ g1_1fin = '/Volumes/group_dv/personal/DValenzano/Apr2014/plink/fam_1.1/fem/fam_1
 #        return dict(zip(self.key, self.value))
 
 
-# In[158]:
+# In[361]:
 
 # And this is the new way:
 class Makep(object):
@@ -62,7 +62,7 @@ class Makep(object):
         self.d = dict(zip(self.key, self.value))
 
 
-# In[182]:
+# In[362]:
 
 g7m = Makep(g7min)
 g7f = Makep(g7fin)
@@ -76,12 +76,12 @@ g1_1f = Makep(g1_1fin)
 
 ##### Here the list of columns that need to be excluded as they are relative to the epistatic interacting markers, and then the list of groups that need to be considered
 
-# In[140]:
+# In[363]:
 
 ems = [g.split('\n')[0].split(',').index(i) for i in g.split('\n')[0].split(',') if 'e' in i]
 
 
-# In[145]:
+# In[364]:
 
 ','.join([g.split('\n')[0].split(',')[i] for i in range(15) if i not in ems])
 
@@ -90,23 +90,23 @@ ems = [g.split('\n')[0].split(',').index(i) for i in g.split('\n')[0].split(',')
 
 # reg = [[j.split(',')[i] for i in range(15) if i not in ems] for j in g.split('\n')[:-1] ]
 
-# In[178]:
+# In[365]:
 
 reg2 = [[reg[0][i][:-2] if j[i] == '1' else j[i]  for i in range(len(reg[0])) ] for j in reg[1:]]
 
 
-# In[188]:
+# In[366]:
 
 d2 = {'g7m':g7m.d,'g7f':g7f.d,'g14m':g14m.d,'g14f':g14f.d,'g8m':g8m.d,'g8f':g8f.d,'g1_1m':g1_1m.d,'g1_1f':g1_1f.d}
 
 
-# In[218]:
+# In[367]:
 
 pvalt = [reg[0]]+[ [j[0]] + [str(d2[j[i]][j[0]]) if j[i] != '0' else '-' for i in range(1, len(reg[0])) ] for j in reg2]
 pvaltab = ','.join([ ','.join(i)+'\n' for i in pvalt]).replace('\n,','\n')
 
 
-# In[230]:
+# In[368]:
 
 z = open('/Volumes/group_dv/personal/DValenzano/Jun2014/Ggroups_pvals.csv', 'w')
 z.write(pvaltab)
@@ -115,7 +115,7 @@ z.close()
 
 #### Now the same will apply to cross AA
 
-# In[223]:
+# In[369]:
 
 aa1min = '/Volumes/group_dv/personal/DValenzano/Apr2014/plink/AAo/mal/fam_1m_sqtl_sig'
 aa1fin = '/Volumes/group_dv/personal/DValenzano/Apr2014/plink/AAo/fem/fam_1f_sqtl_sig'
@@ -123,7 +123,7 @@ aa3min = '/Volumes/group_dv/personal/DValenzano/Apr2014/plink/AAo/mal/fam_3m_sqt
 aa3fin = '/Volumes/group_dv/personal/DValenzano/Apr2014/plink/AAo/fem/fam_3f_sqtl_sig'
 
 
-# In[224]:
+# In[370]:
 
 aa1m = Makep(aa1min)
 aa1f = Makep(aa1fin)
@@ -131,7 +131,7 @@ aa3m = Makep(aa3min)
 aa3f = Makep(aa3fin)
 
 
-# In[225]:
+# In[371]:
 
 aaems = [aa.split('\n')[0].split(',').index(i) for i in aa.split('\n')[0].split(',') if 'e' in i]
 
@@ -139,25 +139,25 @@ aaems = [aa.split('\n')[0].split(',').index(i) for i in aa.split('\n')[0].split(
 # aareg is the new aa matrix, with only the markers without epistatic interaction  
 # aareg2 is the same as aareg, with the names of the groups with significant markers instead of the '1'
 
-# In[227]:
+# In[372]:
 
 aareg = [[j.split(',')[i] for i in range(len(aa.split('\n')[0].split(','))) if i not in ems] for j in aa.split('\n')[:-1] ]
 aareg2 = [[aareg[0][i][:-2] if j[i] == '1' else j[i]  for i in range(len(aareg[0])) ] for j in aareg[1:]]
 
 
-# In[228]:
+# In[373]:
 
 aareg2[:4]
 
 
-# In[229]:
+# In[374]:
 
 aad2 = {'aa1m':aa1m.d,'aa1f':aa1f.d,'aa3m':aa3m.d,'aa3f':aa3f.d}
 aapvalt = [aareg[0]]+[ [j[0]] + [str(aad2[j[i]][j[0]]) if j[i] != '0' else '-' for i in range(1, len(aareg[0])) ] for j in aareg2]
 aapvaltab = ','.join([ ','.join(i)+'\n' for i in aapvalt]).replace('\n,','\n')
 
 
-# In[231]:
+# In[375]:
 
 z = open('/Volumes/group_dv/personal/DValenzano/Jun2014/AAgroups_pvals.csv', 'w')
 z.write(aapvaltab)
@@ -168,24 +168,26 @@ z.close()
 
 # I need to add a column with the number of degrees of freedom, and one for the Xsquared associated 
 
-# In[234]:
+# In[376]:
 
 ghead = pvaltab.split('\n')[0]+',df,Chisq\n'
 aahead = aapvaltab.split('\n')[0]+',df,Chisq\n'
 
 
-# In[235]:
+# In[377]:
 
 import math
 import numpy
 
 
-# In[253]:
+# In[378]:
 
 #chisq = -2*sum([math.log(float(i)) if i!= '-' else 0.0 for i in pvaltab.split('\n')[8].split(',')[1:]])
 
 
-# In[329]:
+##### Here below I apply Fisher's method (of Fisher's combined probability test)
+
+# In[379]:
 
 def xsq(row):
     ls = []
@@ -206,17 +208,17 @@ def wholetab(tab):
     #return ls
 
 
-# In[330]:
+# In[380]:
 
 gtab = wholetab(pvaltab)
 
 
-# In[332]:
+# In[381]:
 
 aatab = wholetab(aapvaltab)
 
 
-# In[335]:
+# In[382]:
 
 z = open('/Volumes/group_dv/personal/DValenzano/Jun2014/Ggroups_chisq.csv','w')
 z.write(gtab)
@@ -227,20 +229,27 @@ z.write(aatab)
 z.close()
 
 
-# In[344]:
+# In[383]:
+
+#def addChisq(inp):
+#    ls = [i + ','+ str(chisqprob(float(i.split(',')[-1]), float(i.split(',')[-2]))) for i in gtab.split('\n')[1:-1]]
+#    return inp.split('\n')[0]+',pval\n'+','.join([i+'\n' for i in ls]).replace('\n,','\n')
+
+
+# In[384]:
 
 def addChisq(inp):
-    ls = [i + ','+ str(chisqprob(float(i.split(',')[-1]), float(i.split(',')[-2]))) for i in gtab.split('\n')[1:-1]]
+    ls = [i + ','+ str(chisqprob(float(i.split(',')[-1]), float(i.split(',')[-2]))) for i in inp.split('\n')[1:-1]]
     return inp.split('\n')[0]+',pval\n'+','.join([i+'\n' for i in ls]).replace('\n,','\n')
 
 
-# In[347]:
+# In[385]:
 
 gX = addChisq(gtab)
 aaX = addChisq(aatab)
 
 
-# In[349]:
+# In[386]:
 
 z = open('/Volumes/group_dv/personal/DValenzano/Jun2014/G_sqtl_chisqpval.csv','w')
 z.write(gX)
@@ -253,7 +262,7 @@ z.close()
 
 # Now for cross G, I will also calculate the p values without considering fam1_1 females, which have surprisingly too many significant markers
 
-# In[354]:
+# In[387]:
 
 def xsq_2(row):
     ls = []
@@ -277,7 +286,7 @@ gtab_2 = wholetab_2(pvaltab)
 g_2X = addChisq(gtab_2)
 
 
-# In[355]:
+# In[388]:
 
 z = open('/Volumes/group_dv/personal/DValenzano/Jun2014/G_sqtl_chisqpval_2.csv','w')
 z.write(g_2X)
