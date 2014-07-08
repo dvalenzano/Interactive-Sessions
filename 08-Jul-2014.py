3e63f2e2aafc65cdcc5471dcbc5cfe8942ac4140
dario valenzano <drvalenzano@gmail.com>
2014-07-08 20:27:22 +0200

    Update 08-Jul-2014.py
    Finds overlapping QTL in non-rqtl markers

--- 08-Jul-2014.py
+++ 08-Jul-2014.py
@@ -48,38 +48,6 @@ glist = ['g7m', 'ge7m', 'g7f', 'ge7f', 'g14m', 'ge14m', 'g14f', 'ge14f', 'g8m', 
 
 # Below the R input for manhattan plot
 
-# In[188]:
-
-class sortColumn2(object):
-
-    def __init__(self, inp, pval, col):
-        self.inp = [i.split(',') for i in inp.replace('-','0_0').split('\n')[:-1]]
-        self.pval = pval # this is the file with the pvalues for each marker
-        self.col = self.inp[0].index(col) #this is the column to select
-        self.srt = sorted(self.inp[1:], key=lambda row: (int(row[self.col].split('_')[0]), float(row[self.col].split('_')[1])))
-        self.srtzip = [i for i in self.srt if i[self.col]!='0_0'] 
-        self.tosave = (','.join(self.inp[0])+'\n'+','.join([','.join(i)+'\n' for i in self.srtzip ])).replace('\n,','\n').replace('0_0','-')
-        self.kg = [ i.split(',')[0] for i in self.pval.split('\n')[1:-2] ]
-        self.kv = [ i.split(',')[-1] for i in self.pval.split('\n')[1:-2] ]
-        self.kd = dict(zip(self.kg, self.kv))
-        self.t = self.tosave.split('\n')
-        self.t2 = [','.join([i.split(',')[0]]+i.split(',')[1].split('_')[:2]+[kd[i.split(',')[0]]]) for i in self.t[1:-1]]
-        self.t3 = [j.split(',')[0] for j in [i for i in self.t[1:-1] if i.split(',')[15]!= ''] if sum(map(float, j.split(',')[15:]))<4] 
-        self.t4 = [k.split(',')[0] for k in  [j for j in [i for i in self.t[1:-1] if i.split(',')[15]!= ''] if sum(map(float, j.split(',')[15:]))<4] if k.split(',')[14]=='-' ]
-        self.ls = []
-    
-    def rinput(self, inp):        
-        for i in inp: 
-            if i.split(',')[0] in self.t3:
-                if i.split(',')[0] in self.t4:
-                    self.ls.append(i+',rf\n')
-                else:    
-                    self.ls.append(i+',rf_rqtl\n')                    
-            else:
-                self.ls.append(i+',na\n')
-        return 'SNP,CHR,BP,P,GROUP\n'+','.join(self.ls).replace('\n,','\n')
-
-
 # In[204]:
 
 class sortColumn2(object):
