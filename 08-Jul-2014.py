fbd5b33f40ce53f0e6419d319a873f215087daab
dario valenzano <drvalenzano@gmail.com>
2014-07-09 17:56:39 +0200

    Update 08-Jul-2014.py

--- 08-Jul-2014.py
+++ 08-Jul-2014.py
@@ -5,7 +5,7 @@
 
 # First, I need to sort grqtl.csv and aarqt.csv based on all the columns except the rqtl one
 
-# In[178]:
+# In[1]:
 
 g = open('/Volumes/group_dv/personal/DValenzano/Jul2014/gQTL.csv', 'rU').read()
 aa = open('/Volumes/group_dv/personal/DValenzano/Jul2014/aaQTL.csv', 'rU').read()
@@ -28,7 +28,7 @@ aapval = open('/Volumes/group_dv/personal/DValenzano/Jun2014/AA_sqtl_chisqpval.c
 #        self.tosave = (','.join(self.inp[0])+'\n'+','.join([','.join(i)+'\n' for i in self.srtzip ])).replace('\n,','\n').replace('0_0','-')
 
 
-# In[41]:
+# In[2]:
 
 glist = ['g7m', 'ge7m', 'g7f', 'ge7f', 'g14m', 'ge14m', 'g14f', 'ge14f', 'g8m', 'ge8m', 'g8f', 'ge8f', 'g1_1m']
 
@@ -48,7 +48,7 @@ glist = ['g7m', 'ge7m', 'g7f', 'ge7f', 'g14m', 'ge14m', 'g14f', 'ge14f', 'g8m', 
 
 # Below the R input for manhattan plot
 
-# In[204]:
+# In[3]:
 
 class sortColumn2(object):
 
@@ -63,7 +63,7 @@ class sortColumn2(object):
         self.kv = [ i.split(',')[-1] for i in self.pval.split('\n')[1:-2] ]
         self.kd = dict(zip(self.kg, self.kv))
         self.t = self.tosave.split('\n')
-        self.t2 = [','.join([i.split(',')[0]]+i.split(',')[self.col].split('_')[:2]+[kd[i.split(',')[0]]]) for i in self.t[1:-1]]
+        self.t2 = [','.join([i.split(',')[0]]+i.split(',')[self.col].split('_')[:2]+[self.kd[i.split(',')[0]]]) for i in self.t[1:-1]]
         self.t3 = [j.split(',')[0] for j in [i for i in self.t[1:-1] if i.split(',')[15]!= ''] if sum(map(float, j.split(',')[15:]))<4] 
         self.t4 = [k.split(',')[0] for k in  [j for j in [i for i in self.t[1:-1] if i.split(',')[15]!= ''] if sum(map(float, j.split(',')[15:]))<4] if k.split(',')[14]=='-' ]
         self.ls = []
@@ -80,11 +80,69 @@ class sortColumn2(object):
         return 'SNP,CHR,BP,P,GROUP\n'+','.join(self.ls).replace('\n,','\n')
 
 
-# In[205]:
+# In[4]:
 
 for i in glist:
     ls = '/Volumes/group_dv/personal/DValenzano/Jul2014/gsort_%s.csv' % i
     p = sortColumn2(g, gpval, i)
+    z = open(ls, 'w')
+    z.write(p.tosave)
+    z.close() #this way I can save several files at once
+    lz = '/Volumes/group_dv/personal/DValenzano/Jul2014/rinput_%s.csv' % i
+    z = open(lz, 'w')
+    z.write(p.rinput(p.t2))
+    z.close()
+
+
+##### Now the same, for AA cross
+
+# In[213]:
+
+aa.split('\n')[0]
+
+
+# In[5]:
+
+aalist = ['aa1m','aae1m','aa1f','aae1f','aa3m','aae3m']
+
+
+# In[6]:
+
+class sortColumn3(object):
+
+    def __init__(self, inp, pval, col):
+        self.inp = [i.split(',') for i in inp.replace('-','0_0').split('\n')[:-1]]
+        self.pval = pval # this is the file with the pvalues for each marker
+        self.col = self.inp[0].index(col) #this is the column to select
+        self.srt = sorted(self.inp[1:], key=lambda row: (int(row[self.col].split('_')[0]), float(row[self.col].split('_')[1])))
+        self.srtzip = [i for i in self.srt if i[self.col]!='0_0'] 
+        self.tosave = (','.join(self.inp[0])+'\n'+','.join([','.join(i)+'\n' for i in self.srtzip ])).replace('\n,','\n').replace('0_0','-').replace('-.','0_0.')
+        self.kg = [ i.split(',')[0] for i in self.pval.split('\n')[1:-2] ]
+        self.kv = [ i.split(',')[-1] for i in self.pval.split('\n')[1:-2] ]
+        self.kd = dict(zip(self.kg, self.kv))
+        self.t = self.tosave.split('\n')
+        self.t2 = [','.join([i.split(',')[0]]+i.split(',')[self.col].split('_')[:2]+[self.kd[i.split(',')[0]]]) for i in self.t[1:-1]]
+        self.t3 = [j.split(',')[0] for j in [i for i in self.t[1:-1] if i.split(',')[8]!= ''] if sum(map(float, j.split(',')[8:]))<4] 
+        self.t4 = [k.split(',')[0] for k in  [j for j in [i for i in self.t[1:-1] if i.split(',')[8]!= ''] if sum(map(float, j.split(',')[8:]))<4] if k.split(',')[7]=='-' ]
+        self.ls = []
+    
+    def rinput(self, inp):        
+        for i in inp: 
+            if i.split(',')[0] in self.t3:
+                if i.split(',')[0] in self.t4:
+                    self.ls.append(i+',rf\n')
+                else:    
+                    self.ls.append(i+',rf_rqtl\n')                    
+            else:
+                self.ls.append(i+',na\n')
+        return 'SNP,CHR,BP,P,GROUP\n'+','.join(self.ls).replace('\n,','\n')
+
+
+# In[7]:
+
+for i in aalist:
+    ls = '/Volumes/group_dv/personal/DValenzano/Jul2014/aasort_%s.csv' % i
+    p = sortColumn3(aa,aapval, i)
     z = open(ls, 'w')
     z.write(p.tosave)
     z.close() #this way I can save several files at once
