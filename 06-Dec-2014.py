21459be510714572baf52a60746ffd2276a5888e
dario valenzano <drvalenzano@gmail.com>
2014-12-06 12:36:13 +0100

    Update 06-Dec-2014.py

--- 06-Dec-2014.py
+++ 06-Dec-2014.py
@@ -43,29 +43,32 @@ z.write(pvaldays)
 z.close()
 
 
-# In[49]:
+# In[72]:
 
 # Now I need to restrict the markers to those on LG3
 g_ = open('/Volumes/group_dv/personal/DValenzano/Oct2014/gdays3_tab.csv', 'rU').read()
 g_3 =  [g_.split('\n')[0]]+[i for i in g_.split('\n')[:-1] if i.split(',')[1]=='3.0']
 g_k = [i.split(',')[0] for i in g_3[1:]]
+g_v = [','.join(i.split(',')[1:3]) for i in g_3[1:]]
+g_d = dict(zip(g_k, g_v))
 
 
-# In[55]:
+# In[73]:
+
+pval_k = [ i.split(',')[0] for i in pvaldays.split('\n')[1:-1] ] 
+pval_v = [ i.split(',')[1] for i in pvaldays.split('\n')[1:-1] ] 
+pval_d = dict(zip(pval_k, pval_v))
+
+
+# In[102]:
 
 # Now I need to subset pvaldays to lg3
+pvaldays_lg3 = ','.join(g_.split('\n')[0].split(',')[:3]) +',pval\n'+   ','.join(sorted([ i+','+g_d[i]+','+pval_d[i]+'\n'  for i in g_k ], key=lambda x: float(x.split(',')[2])) ).replace('\n,','\n')
 
-pvaldays_lg3 = pvaldays.split('\n')[0]+'\n'+ ','.join([i+'\n' for i in pvaldays.split('\n')[1:-1] if i.split(',')[0] in g_k ]).replace('\n,','\n')
 
-
-# In[57]:
+# In[103]:
 
 z = open('/Volumes/group_dv/personal/DValenzano/Dec2014/RF/go_dayslg3_pval.csv', 'w')
 z.write(pvaldays_lg3)
 z.close()
-
-
-# In[ ]:
-
-
 
