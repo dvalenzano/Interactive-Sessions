e47a8c882a23408b2cf3ea56f0c0b3eeaec950c1
dario valenzano <drvalenzano@gmail.com>
2014-12-09 10:49:27 +0100

    Create 09-Dec-2014.py

---
+++ 09-Dec-2014.py
@@ -0,0 +1,61 @@
+
+# coding: utf-8
+
+# In[1]:
+
+# Goal: to generate the input file for the selected non-significant markers from LG 3
+
+g_days = open('/Volumes/group_dv/personal/DValenzano/Sep2014/g_days2.csv', 'rU').read()
+g_daysM = open('/Volumes/group_dv/personal/DValenzano/Sep2014/g_daysM2.csv', 'rU').read()
+g_daysF = open('/Volumes/group_dv/personal/DValenzano/Sep2014/g_daysF2.csv', 'rU').read()
+
+
+# In[3]:
+
+g_dayst = [list(i) for i in zip(* [ i.split(',') for i in g_days.split('\n')[:-1]])]
+g_daysMt = [list(i) for i in zip(* [ i.split(',') for i in g_daysM.split('\n')[:-1]])]
+g_daysFt = [list(i) for i in zip(* [ i.split(',') for i in g_daysF.split('\n')[:-1]])]
+
+import numpy
+import scipy
+import math
+
+
+# In[7]:
+
+# Now I need to select which markers to use for the analysis
+# I will use the same as those defined as "peak" in file /Volumes/group_dv/personal/DValenzano/Oct2014/g_lg3_peak.fa
+selected = ['40077', '7106']
+
+m0 = g_daysMt[:3]+[i for i in g_daysMt[3:] if i[0] in selected]
+f0 = g_daysFt[:3]+[i for i in g_daysFt[3:] if i[0] in selected]
+
+
+# In[45]:
+
+m1 = [list(i) for i in zip(*[[i[0]]+i[5:] for i in m0 ])]
+f1 = [list(i) for i in zip(*[[i[0]]+i[5:] for i in f0 ])]
+
+
+# In[46]:
+
+m2 = ','.join([','.join(i)+'\n' for i in m1]).replace('\n,','\n')
+f2 = ','.join([','.join(i)+'\n' for i in f1]).replace('\n,','\n')
+
+
+# In[48]:
+
+z = open('/Volumes/group_dv/personal/DValenzano/Dec2014/m_selectedG.csv', 'w')
+z.write(m2)
+z.close()
+
+z = open('/Volumes/group_dv/personal/DValenzano/Dec2014/f_selectedG.csv', 'w')
+z.write(f2)
+z.close()
+
+
+
+# In[ ]:
+
+
+
