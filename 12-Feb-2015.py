822e5d81364f14dedea5350a6ca114b8284b4580
dario valenzano <drvalenzano@gmail.com>
2015-02-12 17:27:47 +0100

    Create 12-Feb-2015.py

---
+++ 12-Feb-2015.py
@@ -0,0 +1,63 @@
+Goal: to measure heterozygosity in GRZ
+
+
+# In[1]:
+
+c1 = open('/Volumes/group_dv/personal/DValenzano/Nov2013/goped7.csv', 'rU').read()
+c2 = open('/Volumes/group_dv/personal/DValenzano/Nov2013/aaoped7.csv', 'rU').read()
+
+
+# In[25]:
+
+c1_aa = c1.split('\n')[1].split(',')[10:].count('aa')
+c1_bb = c1.split('\n')[1].split(',')[10:].count('bb')
+c1_ab = c1.split('\n')[1].split(',')[10:].count('ab')
+c1_length = len(c1.split('\n')[1].split(',')[10:])
+
+
+# In[26]:
+
+from sets import Set
+Set(c1.split('\n')[1].split(',')[10:])
+
+
+# In[27]:
+
+c1_length == c1_aa + c1_ab
+
+
+# In[30]:
+
+fc1_aa = float(c1_aa)/float(c1_length)
+fc1_ab = float(c1_ab)/float(c1_length)
+
+
+# In[31]:
+
+fc1_ab
+
+
+# the above value is the heterozygosity for females (GRZ) for cross 1
+# Now we need to find the value for males (cross 2)
+
+# In[34]:
+
+c2_aa = c2.split('\n')[2].split(',')[10:].count('aa')
+c2_bb = c2.split('\n')[2].split(',')[10:].count('bb')
+c2_ab = c2.split('\n')[2].split(',')[10:].count('ab')
+c2_length = len(c2.split('\n')[1].split(',')[10:])
+
+from sets import Set
+Set(c2.split('\n')[2].split(',')[10:])
+
+
+# In[36]:
+
+fc2_ab = float(c2_ab)/float(c2_length)
+fc2_ab
+
+
+# In[ ]:
+
+
+
