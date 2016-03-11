d4024a85f447b996cbff1588e08fc63d4b8a5595
dario valenzano <drvalenzano@gmail.com>
2016-03-11 14:21:58 +0100

    Update 11-Mar-2016.py

--- 11-Mar-2016.py
+++ 11-Mar-2016.py
@@ -1,5 +1,4 @@
 
-## GOAL: reformat fkpm and OTU tables for microbiome study - these generate R input files to compute multiple regression
 # coding: utf-8
 
 # In[3]:
@@ -16,46 +15,48 @@ z.write(t)
 z.close()
 
 
-# In[25]:
+# In[85]:
 
-t = t.replace('Nex.Reference', '')
+t = t.replace('New.Reference', '')
 t2 = t.split('\n')[1:]
 t3 = [ i.split(',') for i in t2 ]
-tt = zip(*t3)
+t4 = [t3[0]]+[i for i in t3[1:] if sum(map(float, (i[1:]))) != 0.0] #this removes 0-only OTUs
+tt = zip(*t4)
 tT = [ list(i) for i in tt ]
 
 
-# In[8]:
+# In[86]:
 
 u = open('/Volumes/group_dv/personal/DValenzano/papers/uBiome0/data/rnaseq_fpkm_genes.csv', 'rU').read()
 u2 = u.split('\n')
 
 
-# In[26]:
+# In[87]:
 
 u3 = [ i.split(',') for i in u2]
-ut = zip(*u3)
+u4 = [u3[0]]+[i for i in u3[1:] if sum(map(float, (i[1:]))) != 0.0] #this removes 0-only transcripts
+ut = zip(*u4)
 uT = [ list(i) for i in ut ]
 
 
-# In[44]:
+# In[88]:
 
 uTs = [uT[0]] + sorted(uT[1:], key = lambda x: (x[0]))
 tTs = [tT[0]] + sorted(tT[1:], key = lambda x: (x[0]))
 
 
-# In[45]:
+# In[89]:
 
 [i[0] for i in uTs][1:] == [i[0] for i in tTs][1:] # this tests whether the two matrices have headers in same order
 
 
-# In[68]:
+# In[90]:
 
 uj = ','.join([','.join(i)+'\n' for i in uTs]).replace('\n,','\n')[:-1]
 tj = ','.join([','.join(i)+'\n' for i in tTs]).replace('\n,','\n')[:-1]
 
 
-# In[69]:
+# In[91]:
 
 zu = open('/Volumes/group_dv/personal/DValenzano/papers/uBiome0/data/rnaseq_fpkm_genes_sorted.csv', 'w')
 zu.write(uj)
@@ -64,9 +65,5 @@ zu.close()
 zt = open('/Volumes/group_dv/personal/DValenzano/papers/uBiome0/data/rnaseq_merged_otu_table_sorted.csv', 'w')
 zt.write(tj)
 zt.close()
-
-
-# In[ ]:
-
 
 
